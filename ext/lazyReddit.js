984809ca2b71e1a3b12b4c22556094f71d726bb7
Ignacio Ampuero <iampuero@findthebest.com>
2014-12-21 23:58:09 -0300

    Working commit post airplane

--- ext/lazyReddit.js
+++ ext/lazyReddit.js
@@ -1,14 +1,11 @@
-// Note: these commits were done in an airplane with no internet
-// so there is a very good chance it is broken right now.
+var Comment = function(json) {
 
-var Comment = function(data) {
-
-	init = function(json) {
-		this.data = $.extend(json.data);
+	this.init = function(json) {
+		this.data = json.data;
 	};
 
 	// Fuck this function.
-	toHtml = function() {
+	this.toHtml = function() {
 		var d  = this.data;
 		return '' +
 			'<div class="comment thing" id=' + d.id + '>' +
@@ -16,46 +13,8 @@ var Comment = function(data) {
 				'<div class="entry">' + d.body_html + '</div>' +
 			'</div>';
 	};
-};
 
-var rCommentsController = {
-
-	model : rCommentsModel,
-	view : rCommentsView,
-	request : new XMLHttpRequest(),
-
-	init : function() {
-		var self = this;
-		$('body')
-			.on('mouseover', 'a.comments', function() {
-				self.renderComment($(this));
-			})
-			.on('mouseleave', 'a.comments', function() {
-				self.view.hidePopup();
-			});
-	},
-
-	renderComment : function($el) {
-		var self = this,
-			url = $el.attr('href'),
-			commentId = $el.closest('.thing').attr('id'), // this gonna break
-			requestData = model.getRequestData(url, commentId),
-			request = this.request;
-
-		request.abort();
-
-		if (requestData.cached) {
-			self.view.show(requestData.cached, el);
-			return;
-		}
-
-		request = $.getJSON(requestData).done(function(data) {
-				var commentJson = self.model.registerComment(requestData.url, data, commentId);
-				self.view.show(commentJson, $el);
-			});
-
-		this.request = request;
-	}
+	this.init(json);
 };
 
 var rCommentsView = {
@@ -63,9 +22,8 @@ var rCommentsView = {
 
 	show : function(json, $el) {
 		var comment = new Comment(json),
-			html = comment.toHtml(),
-			popup = this.pqopup($el);
-
+			commentHtml = comment.toHtml(),
+			popup = this.popup($el);
 		popup.html(commentHtml);
 		popup.show();
 	},
@@ -103,8 +61,8 @@ var rCommentsView = {
 
 var rCommentsModel = {
 
-	commentCache : [],
-	commentStatus : [],
+	commentCache : {},
+	commentStatus : {},
 
 	getRequestData : function(url, commentId) {
 		var key = this.genKey(url, commentId),
@@ -163,6 +121,47 @@ var rCommentsModel = {
 
 	genKey : function(url, commentId) {
 		return url + commentId;
+	}
+};
+
+var rCommentsController = {
+
+	model : rCommentsModel,
+	view : rCommentsView,
+	request : new XMLHttpRequest(),
+
+	init : function() {
+		var self = this;
+		$('body')
+			.on('mouseover', 'a.comments', function() {
+				self.renderComment($(this));
+			})
+			.on('mouseleave', 'a.comments', function() {
+				self.view.hidePopup();
+			});
+	},
+
+	renderComment : function($el) {
+		var self = this,
+			url = $el.attr('href') + '.json',
+			commentId = $el.closest('.thing').attr('id'), // this gonna break
+			requestData = self.model.getRequestData(url, commentId),
+			request = self.request,
+			commentJson;
+
+		request.abort();
+
+		if (requestData.cached) {
+			self.view.show(requestData.cached, $el);
+			return;
+		}
+
+		request = $.getJSON(requestData.url, requestData.params).done(function(data) {
+				commentJson = self.model.registerComment(requestData.url, data, commentId);
+				self.view.show(commentJson, $el);
+			});
+
+		this.request = request;
 	}
 };
 
