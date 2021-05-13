b8f8624e9975a68b8ad2b72e61892f8fc9fa95d6
Ignacio Ampuero <ignaciorampuero@gmail.com>
2021-05-13 11:27:02 -0700

    Explicitly pass production/development to webpack environment when building and developingOD

--- source/webpack.config.js
+++ source/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
+	mode: 'production',
 	entry: './rComments.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'rComments.js'
-	}
+		filename: 'rComments.js',
+	},
 };
